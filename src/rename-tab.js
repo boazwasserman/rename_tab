597f548a09b5cb31c22cf6bfa3160e71195ab15e
Boaz Wasserman <boazwasser@gmail.com>
2024-06-04 16:52:02 +0300

    reupdate title

--- src/rename-tab.js
+++ src/rename-tab.js
@@ -3,28 +3,24 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     document.title = request.title;
     chrome.runtime.sendMessage({ request: "getTabId" }, response => {
         const tabId = response.tabId;
-        console.log("Found tab ID:", tabId);
         localStorage.setItem('customTitle_' + tabId, request.title); // Save title to local storage
       });
-    
-    chrome.storage.sync.set({customTitle: request.title}, function() {
-      sendResponse({status: 'title set'});
-    });
   }
 });
 
+function setTabName() {
+    chrome.runtime.sendMessage({ request: "getTabId" }, response => {
+        const tabId = response.tabId;
+        const customTitle = localStorage.getItem('customTitle_' + tabId);
+        if (customTitle) {
+            document.title = customTitle;
+        }
+      });
+}
+
 // Retrieve and set title on load
 window.addEventListener('load', () => {
-  
-  console.log("load");
-
-  // Access the tab ID from the global window object set by background.js
-  chrome.runtime.sendMessage({ request: "getTabId" }, response => {
-    const tabId = response.tabId;
-    console.log("Loaded tab ID:", tabId);
-    const customTitle = localStorage.getItem('customTitle_' + tabId);
-    if (customTitle) {
-        document.title = customTitle;
-    }
-  });
+    // Access the tab ID from the global window object set by background.js
+    setTabName();
+    setInterval(setTabName, 5000);
 });
